9dd953e635d80827b51b9506e6f33ac710332e8e
Mark Pokorny <iridium193@gmail.com>
2015-03-30 16:47:58 +0100

    Added acquisition of visitor name as parameters.

--- hello.rb
+++ hello.rb
@@ -8,3 +8,9 @@ end
 get '/secret' do
   'This is a secret page that should update as soon as the source document is saved.'
 end
+
+get '/hello' do
+  @visitor = params[:name]
+  @name = %w(Amigo Oscar Viking).sample
+  erb :index
+end
